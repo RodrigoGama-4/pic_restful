727146c48e41048a9b1450cc185512de237ba7ca
rodrigogama <rodrigogamah4@gmail.com>
2023-09-04 21:41:01 -0300

    classe de config global e NotificationDTO

---
+++ src/main/java/com/picpaysimplificado/infra/AppConfig.java
@@ -0,0 +1,9 @@
+package com.picpaysimplificado.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+}
